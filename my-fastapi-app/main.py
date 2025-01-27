d0c47508e6a39b4dc9ec89e68580ba1e0606f400
nansystem <sato171786@gmail.com>
2025-01-27 12:33:08 +0900

    fix(fastapi): CORSの設定を削除

--- my-fastapi-app/main.py
+++ my-fastapi-app/main.py
@@ -1,15 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from config.settings import settings
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=settings.cors_origins,
-    allow_credentials=True,
-    allow_methods=settings.cors_methods,
-    allow_headers=settings.cors_headers,
-)
 
 @app.get("/")
 async def root():
